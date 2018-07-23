7123e58701eda635e318108b94841b3b15dbed38
Marsel Gray <marselgray@gmail.com>
2018-07-23 13:49:10 -0400

    bubble

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# algorithm
+# Algorithms: An Introduction 
+
+
+In its most basic defintion an algorithm is a set of rules or instructions. Think of a flow chart. That's an algorithm. A cooking reciepe is an algorithm. There are many types of algorithms. 
+
+This repo contains code for some of the more basic and simple algorithms. 
+
+# Bubble Sort 
+
+Bubble sort is the simplest sorting algorithm. It works by repeatedly swapping the adjacent elements if they are in wrong order.
+![](images/bubble.png)
+
+
+# Measuring An Algorithm's Efficenty
---
+++ bubblesort.py
@@ -0,0 +1,25 @@
+# Bubble Sort
+
+def bubbleSort(array):
+	n = len(array)
+
+	# Traverse through all array elements
+	for i in range(n):
+
+		# Last i elements are already in place
+		for j in range(0, n-i-1):
+
+			# traverse the array from 0 to n-i-1
+			# Swap if the element found is greater
+			# than the next element
+			if array[j] > array[j+1] :
+				array[j], array[j+1] = array[j+1], array[j]
+
+# sample test
+array = [90, 15, 25, 4, 22, -3, 90]
+
+bubbleSort(array)
+
+print ("The sorted array is:")
+for i in range(len(array)):
+	print ("%d" %array[i]), 
